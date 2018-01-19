199fc9d16a44eb71991189ce896ac3f9e16806dc
ringol5046 <ringol5046@gmail.com>
2018-01-18 23:54:21 -0600

    added eslint

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
+
 app.listen(4000, () => {
-  console.log('server starting on localhost 4000')
+  console.log('server starting on localhost 4000');
 });
